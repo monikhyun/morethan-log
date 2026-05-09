92ce4049787b3380af90292bde0a08a87e2999df
문익현 <97443699+monikhyun@users.noreply.github.com>
2026-05-09 12:50:07 +0900

    Normalize page ID before fetching record map

--- src/apis/notion-client/getRecordMap.ts
+++ src/apis/notion-client/getRecordMap.ts
@@ -2,8 +2,15 @@ import { NotionAPI } from "notion-client"
 import { idToUuid } from "notion-utils"
 import { normalizeRecordMap } from "src/libs/utils/notion/normalizeRecordMap"
 
+const normalizePageId = (pageId: string) => {
+  return pageId.replace(/-/g, "")
+}
+
 export const getRecordMap = async (pageId: string) => {
   const api = new NotionAPI()
-  const recordMap = normalizeRecordMap(await api.getPage(idToUuid(pageId)))
+
+  const safePageId = normalizePageId(pageId)
+  const recordMap = normalizeRecordMap(await api.getPage(idToUuid(safePageId)))
+
   return recordMap
 }
